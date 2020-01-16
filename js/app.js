221c6e86781cb2a6d07c6d6de7b0149aa0e5062d
Uodeeb <uodeeb@gmail.com>
2020-01-16 16:46:56 +0200

    Build the Home Pgae Structure and Features

---
+++ js/app.js
@@ -0,0 +1,27 @@
+let showContent=function(){
+    let buttons = document.getElementsByClassName("butt");
+    const cards=document.getElementsByClassName('card');
+Array.from(buttons).forEach(butt=>{
+    console.log("show content is working");
+    console.log(butt.parentElement);
+    butt.addEventListener('click', ()=>{
+
+        console.log(cards);
+        Array.from(cards).forEach(card=>{
+            console.log(card);
+         console.log(card.dataset.nav);
+         if(butt.dataset.nav === card.dataset.nav){
+            card.style.display="block";
+         }else{
+            card.style.display="none";
+         }
+         
+        })
+    })
+})
+    
+
+
+
+}
+showContent();

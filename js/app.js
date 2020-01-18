2cb44f2cf1f7756c6b6efb12e2f31115397acce4
Uodeeb <uodeeb@gmail.com>
2020-01-18 22:16:03 +0200

    Finish The Main Structure

--- js/app.js
+++ js/app.js
@@ -1,27 +1,28 @@
-let showContent=function(){
-    let buttons = document.getElementsByClassName("butt");
-    const cards=document.getElementsByClassName('card');
-Array.from(buttons).forEach(butt=>{
-    console.log("show content is working");
-    console.log(butt.parentElement);
-    butt.addEventListener('click', ()=>{
+// let showContent=function(){
+//     let buttons = document.getElementsByClassName("butt");
+//     const cards=document.getElementsByClassName('card');
+// Array.from(buttons).forEach(butt=>{
+//     console.log("show content is working");
+//     console.log(butt.parentElement);
+//     butt.addEventListener('click', ()=>{
 
-        console.log(cards);
-        Array.from(cards).forEach(card=>{
-            console.log(card);
-         console.log(card.dataset.nav);
-         if(butt.dataset.nav === card.dataset.nav){
-            card.style.display="block";
-         }else{
-            card.style.display="none";
-         }
+//         console.log(cards);
+//         Array.from(cards).forEach(card=>{
+//             console.log(card);
+//          console.log(card.dataset.nav);
+//          if(butt.dataset.nav === card.dataset.nav){
+//              document.getElementById('main_img').style.display="none";  
+//             card.style.display="block";
+//          }else{
+//             card.style.display="none";
+//          }
          
-        })
-    })
-})
+//         })
+//     })
+// })
     
 
 
 
-}
-showContent();
+// }
+// showContent();
